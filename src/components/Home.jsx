4bcbbf45d83fb93807f9389fa0afd1e220fa5413
jknithin36 <jknithin36@gmail.com>
2024-06-01 21:40:23 -0400

    added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,20 @@
 export default function Main() {
-    return (
-        <div
-            id="home"
-            className="page flex min-h-screen flex-col items-center justify-center bg-white"
-        >
-            <div className="text-center text-black">
-                <h1 className="font-chivo animate-pulse bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 bg-clip-text text-[160px] uppercase text-transparent opacity-30">
-                    Nithin
-                </h1>
-                <h3 className="font-rubik animate-fade-in-up text-[200px] uppercase text-black">
-                    PORTFOLIO
-                </h3>
-                <p className="font-caveat animate-fade-in-down py-2 text-black">
-                    WEB DEVELOPER
-                </p>
-            </div>
-        </div>
-    )
+  return (
+    <div
+      id="home"
+      className="page flex min-h-screen flex-col items-center justify-center bg-white"
+    >
+      <div className="text-center text-black">
+        <h1 className="font-chivo animate-pulse bg-gradient-to-r from-yellow-400 via-red-500 to-pink-500 bg-clip-text text-transparent opacity-30 text-[40px] sm:text-[80px] md:text-[120px] lg:text-[160px] uppercase">
+          Nithin
+        </h1>
+        <h3 className="font-rubik animate-fade-in-up text-[50px] sm:text-[100px] md:text-[150px] lg:text-[200px] uppercase text-black">
+          PORTFOLIO
+        </h3>
+        <p className="font-caveat animate-fade-in-down py-2 text-black text-[16px] sm:text-[24px] md:text-[32px] lg:text-[40px]">
+          WEB DEVELOPER
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,83 +1,85 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 function Skills() {
-    const [fadeIn, setFadeIn] = useState(false)
+  const [fadeIn, setFadeIn] = useState(false);
 
-    useEffect(() => {
-        setFadeIn(true)
-    }, [])
+  useEffect(() => {
+    setFadeIn(true);
+  }, []);
 
-    return (
-        <div
-            id="skills"
-            className={`flex min-h-screen bg-white ${fadeIn ? 'fade-in' : ''}`}
-        >
-            <div className="flex w-1/2 flex-col items-center justify-center px-8">
-                <div className="mt-8 max-w-4xl">
-                    <h1 className="font-rubik mb-8 text-6xl uppercase text-gray-800">
-                        Skills
-                    </h1>
-                    <SkillCategory
-                        title="Programming Languages"
-                        skills={['JavaScript', 'Python (Familiar)']}
-                    />
-                    <SkillCategory
-                        title="Frontend Development"
-                        skills={[
-                            'HTML',
-                            'CSS',
-                            'React.js',
-                            'React Native',
-                            'Next.js',
-                            'Bootstrap',
-                            'Tailwind CSS',
-                            'Redux',
-                            'React Router',
-                        ]}
-                    />
-                </div>
-            </div>
-            <div className="flex w-1/2 flex-col items-center justify-center px-8">
-                <div className="mt-8 max-w-4xl">
-                    <SkillCategory
-                        title="Backend Development"
-                        skills={[
-                            'Node.js',
-                            'Express.js',
-                            'MongoDB',
-                            'RESTful APIs',
-                            'Flask ',
-                            'mongoose',
-                        ]}
-                    />
-                    <SkillCategory
-                        title="Other Skills"
-                        skills={['Git', 'Canva (Graphic Design)']}
-                    />
-                </div>
-            </div>
+  return (
+    <div
+      id="skills"
+      className={`flex min-h-screen flex-col md:flex-row bg-white ${
+        fadeIn ? "fade-in" : ""
+      }`}
+    >
+      <div className="flex w-full md:w-1/2 flex-col items-center justify-center px-4 md:px-8">
+        <div className="mt-8 max-w-4xl">
+          <h1 className="font-rubik mb-8 text-3xl sm:text-4xl md:text-5xl lg:text-6xl uppercase text-gray-800">
+            Skills
+          </h1>
+          <SkillCategory
+            title="Programming Languages"
+            skills={["JavaScript", "Python (Familiar)"]}
+          />
+          <SkillCategory
+            title="Frontend Development"
+            skills={[
+              "HTML",
+              "CSS",
+              "React.js",
+              "React Native",
+              "Next.js",
+              "Bootstrap",
+              "Tailwind CSS",
+              "Redux",
+              "React Router",
+            ]}
+          />
         </div>
-    )
+      </div>
+      <div className="flex w-full md:w-1/2 flex-col items-center justify-center px-4 md:px-8">
+        <div className="mt-8 max-w-4xl">
+          <SkillCategory
+            title="Backend Development"
+            skills={[
+              "Node.js",
+              "Express.js",
+              "MongoDB",
+              "RESTful APIs",
+              "Flask ",
+              "mongoose",
+            ]}
+          />
+          <SkillCategory
+            title="Other Skills"
+            skills={["Git", "Canva (Graphic Design)"]}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 function SkillCategory({ title, skills }) {
-    return (
-        <div className="mb-12">
-            <h2 className="font-rubik mb-4 text-3xl font-bold text-gray-800">
-                {title}
-            </h2>
-            <div className="grid grid-cols-3 gap-6">
-                {skills.map((skill, index) => (
-                    <div
-                        key={index}
-                        className="animate-fade-in flex items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-6 shadow-md"
-                    >
-                        {skill}
-                    </div>
-                ))}
-            </div>
-        </div>
-    )
+  return (
+    <div className="mb-12">
+      <h2 className="font-rubik mb-4 text-xl sm:text-2xl md:text-3xl font-bold text-gray-800">
+        {title}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
+        {skills.map((skill, index) => (
+          <div
+            key={index}
+            className="animate-fade-in flex items-center justify-center rounded-md border border-gray-200 bg-gray-100 p-4 sm:p-6 shadow-md"
+          >
+            {skill}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Skills
+export default Skills;
